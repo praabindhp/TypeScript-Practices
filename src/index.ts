529eccf1eee3f1b8deafa5398cfeb0005f37efe4
praabindh <praabindh@codingmart.com>
2023-06-16 18:43:05 +0530

    Updated TypeScript Practices

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-console.log("Victory To The Blood Of Jesus");
-let age: number = 20;
-let word: string = "Victory";
+// let age: number = 21;
+// if (age < 50) age += 10;
+// console.log(age);
+
+let sales = 100;
+let course = "TypeScript";
+let is_published = true;
+let level;
+
+function render(document: any) {
+    console.log(document)
+}
